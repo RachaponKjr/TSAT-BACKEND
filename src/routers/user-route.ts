7177ea704d9e2d94a70f0e8adb18fd519af00833
RachaponKjr <rachapon.navy@gmail.com>
2025-05-19 02:13:21 +0700

    fix

--- src/routers/user-route.ts
+++ src/routers/user-route.ts
@@ -9,7 +9,7 @@ import {
 import { authenticateToken, isAdmin, isOwner } from '../middlewares/auth-admin';
 const router = express.Router();
 
-router.post('/create-user', createUserController);
+router.post('/create-user', authenticateToken, isOwner, createUserController);
 router.get('/get-users', authenticateToken, isAdmin, getUserController);
 router.delete(
   '/delete-user/:id',
